77e6ffb9c62e04caf7659d12d7322e140253e5ca
Tommy Lee <tommy.t.lee@gmail.com>
2015-09-06 00:34:36 +0000

    Add Invoice Points Spec

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,7 @@ class Invoice < ActiveRecord::Base
   belongs_to :person
   DOLLAR_POINTS_CONVERSION = 10
   def calculate_points
-    points = (total * DOLLAR_POINTS_CONVERSION).floor
-    points
+    self.points = (total * DOLLAR_POINTS_CONVERSION).floor
+    self.points
   end
 end
