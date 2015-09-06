b83083e9f6bf4bdcf8d60dda6f8ba7388d8b50e1
Tommy Lee <tommytlee@gmail.com>
2015-09-06 04:27:46 +0000

    Add Null Case to Invoice Test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,10 @@ class Invoice < ActiveRecord::Base
   belongs_to :person
   DOLLAR_POINTS_CONVERSION = 10
   def calculate_points
-    self.points = (total * DOLLAR_POINTS_CONVERSION).floor
+    if self.total.nil?
+      self.total = 0
+    end
+    self.points = (self.total * DOLLAR_POINTS_CONVERSION).floor
     self.points
   end
 end
